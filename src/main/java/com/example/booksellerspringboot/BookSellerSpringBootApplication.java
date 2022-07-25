a189066f3be404d18fb11862e3a9576dd40eb6bd
kenwoo9y <ei.1px.9.01@gmail.com>
2022-07-25 23:32:32 +0900

    first commit

---
+++ src/main/java/com/example/booksellerspringboot/BookSellerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksellerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSellerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSellerSpringBootApplication.class, args);
+	}
+
+}
