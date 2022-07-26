48e6f19479d9951bc8d04ad1291f3f2337f4fb31
kenwoo9y <ei.1px.9.01@gmail.com>
2022-07-27 00:45:20 +0900

    add Book class

---
+++ src/main/java/com/example/booksellerspringboot/entity/Book.java
@@ -0,0 +1,19 @@
+package com.example.booksellerspringboot.entity;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private String id;
+    private String name;
+    private String description;
+    private Double price;
+    private LocalDateTime createdTime;
+    private LocalDateTime updatedTime;
+}
