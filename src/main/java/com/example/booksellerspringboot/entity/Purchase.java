0e1dbc1984cb3171e4e3b83c2a55f0d3b67d49ea
kenwoo9y <ei.1px.9.01@gmail.com>
2022-07-27 00:47:19 +0900

    add Purchase class

---
+++ src/main/java/com/example/booksellerspringboot/entity/Purchase.java
@@ -0,0 +1,18 @@
+package com.example.booksellerspringboot.entity;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Purchase {
+    private String id;
+    private String userId;
+    private String bookId;
+    private Double price;
+    private LocalDateTime purchaseDateTime;
+}
