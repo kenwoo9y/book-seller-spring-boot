f7387103c1d41a0bbe8de2accfdf650d41f3c6e6
kenwoo9y <ei.1px.9.01@gmail.com>
2022-07-26 23:42:52 +0900

    add User class

---
+++ src/main/java/com/example/booksellerspringboot/entity/User.java
@@ -0,0 +1,22 @@
+package com.example.booksellerspringboot.entity;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String id;
+    private String userName;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private LocalDateTime createdTime;
+    private LocalDateTime updatedTime;
+    private Role role;
+}
