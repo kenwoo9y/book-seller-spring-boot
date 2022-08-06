14488248025e846dcb9e3e409d9c5d4932bba979
kenwoo9y <ei.1px.9.01@gmail.com>
2022-08-06 20:21:57 +0900

    add necessary annotations to BookServiceImpl

--- src/main/java/com/example/booksellerspringboot/service/BookServiceImpl.java
+++ src/main/java/com/example/booksellerspringboot/service/BookServiceImpl.java
@@ -4,10 +4,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.booksellerspringboot.entity.Book;
 import com.example.booksellerspringboot.repository.BookRepository;
 
+@Service
 public class BookServiceImpl implements BookService {
     @Autowired
     BookRepository bookRepository;
@@ -25,6 +28,7 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
+    @Transactional
     public void deleteBookById(String bookId) {
         this.bookRepository.deleteBookById(bookId);
     }
