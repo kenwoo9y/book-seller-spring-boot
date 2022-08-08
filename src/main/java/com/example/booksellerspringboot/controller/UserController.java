bac8dd5c3d08f76b253b411fbfac8a0b38e032ae
kenwoo9y <ei.1px.9.01@gmail.com>
2022-08-08 23:48:24 +0900

    add UserController class

---
+++ src/main/java/com/example/booksellerspringboot/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.example.booksellerspringboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.booksellerspringboot.entity.User;
+import com.example.booksellerspringboot.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    
+    @PostMapping
+    public ResponseEntity<?> saveUser(@RequestBody User user) {
+        userService.saveUser(user);
+        
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getUserByUserName(String userName) {
+        return new ResponseEntity<>(userService.getUserByUserName(userName), HttpStatus.OK);
+    }
+    
+}
