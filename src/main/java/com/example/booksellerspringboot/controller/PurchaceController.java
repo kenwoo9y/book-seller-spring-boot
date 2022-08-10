8725a30a2df53a9e313b0313c67b67d6e169625b
kenwoo9y <ei.1px.9.01@gmail.com>
2022-08-10 23:44:33 +0900

    add PurchaceController class

---
+++ src/main/java/com/example/booksellerspringboot/controller/PurchaceController.java
@@ -0,0 +1,26 @@
+package com.example.booksellerspringboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.booksellerspringboot.entity.Purchase;
+import com.example.booksellerspringboot.service.PurchaseService;
+
+@RestController
+@RequestMapping("/api/purchase")
+public class PurchaceController {
+    @Autowired
+    private PurchaseService purchaseService;
+
+    @PostMapping
+    public ResponseEntity<?> savePurchase(@RequestBody Purchase purchase) {
+        purchaseService.savePurchase(purchase);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
