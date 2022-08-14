e170b0b51c3c77fce0065a503320813d3c561418
kenwoo9y <ei.1px.9.01@gmail.com>
2022-08-14 17:08:32 +0900

    add Service annotation

--- src/main/java/com/example/booksellerspringboot/service/PurchaseServiceImpl.java
+++ src/main/java/com/example/booksellerspringboot/service/PurchaseServiceImpl.java
@@ -4,10 +4,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.booksellerspringboot.entity.Purchase;
 import com.example.booksellerspringboot.repository.PurchaseRepository;
 
+@Service
 public class PurchaseServiceImpl implements PurchaseService {
     @Autowired
     PurchaseRepository purchaseRepository;
