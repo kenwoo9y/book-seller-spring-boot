267814d1172279e29da73c7b63eada0b6971f3a1
kenwoo9y <ei.1px.9.01@gmail.com>
2022-08-31 23:39:53 +0900

    add AuthenticationController class

---
+++ src/main/java/com/example/booksellerspringboot/controller/AuthenticationController.java
@@ -0,0 +1,36 @@
+package com.example.booksellerspringboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.booksellerspringboot.entity.User;
+import com.example.booksellerspringboot.service.AuthenticationService;
+import com.example.booksellerspringboot.service.UserService;
+
+@RestController
+@RequestMapping("/api/authentication")
+public class AuthenticationController {
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("sign-up")
+    public ResponseEntity<?> signUp(@RequestBody User user) {
+        if (userService.getUserByUserName(user.getUserName()).isPresent()) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping("sign-in")
+    public ResponseEntity<?> signIn(@RequestBody User user) {
+        return new ResponseEntity<>(authenticationService.signinAndReturnJwt(user), HttpStatus.OK);
+    }
+}
